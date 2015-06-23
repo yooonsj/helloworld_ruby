36bc1fe7614cdc8db57777165317f972c432c58e
YoonSangjun <yooonsj@gmail.com>
2015-06-23 15:35:17 +0900

    함수

---
+++ function/ex03.rb
@@ -0,0 +1,15 @@
+# 입력값
+def a(num)
+  return 'a' * num
+end
+
+puts(a(3))
+
+puts('=====================')
+
+# 여러개의 입력값
+def make_string(str, num)
+  return str * num
+end
+
+puts(make_string('a', 3))
---
+++ function/ex04.rb
@@ -0,0 +1,19 @@
+puts('아이디를 입력하세요.')
+input_id = gets.chomp()
+
+def login(_id)
+  members = ['egoing', 'k8805', 'leezche']
+  for member in members
+    if member == _id
+      return true
+    end
+  end
+
+  return false
+end
+
+if login(input_id)
+  puts('Hello!, ' + input_id)
+else
+  puts('Who are you?')
+end
