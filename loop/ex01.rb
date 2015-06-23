0b2bc002806162269a5bb53262d9730e55f7d24a
YoonSangjun <yooonsj@gmail.com>
2015-06-23 13:43:12 +0900

    반복문

---
+++ loop/ex01.rb
@@ -0,0 +1,11 @@
+while false do
+  puts('Hello world!')
+end
+puts('after while')
+
+# 반복문 조건
+i = 0
+while i < 3 do
+  puts('Hello world')
+  i = i + 1
+end
---
+++ loop/ex03.rb
@@ -0,0 +1,18 @@
+i = 0
+while i < 10 do
+  if i == 4
+    puts(i)
+  end
+  i = i + 1
+end
+
+puts('=================')
+i = 0
+while i < 10 do
+  if i == 4
+    break
+  end
+  puts(i)
+  i = i + 1
+end
+puts('after while')
